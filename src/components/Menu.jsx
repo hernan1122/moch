c387192f0345eb97a8bc2cec026eb0b4fbc63809
Axel Torrico <torricohernan2@gmail.com>
2023-06-09 14:47:28 -0300

    cree el menu y le di estilos

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import '../styles/Menu.css'
+
+//icons
+import { VscHome } from 'react-icons/vsc'
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdFavoriteBorder } from "react-icons/md";
 
 export function Menu() {
   return (
     <div className='Menu'>
-      
+      <NavLink className='Menu-link' to='/' activeclassname="active">
+        <VscHome className='Menu-icons' />
+        <h5>Inicio</h5>
+      </NavLink>
+      <NavLink className='Menu-link' to='/search' activeclassname="active">
+        <AiOutlineSearch className='Menu-icons' />
+        <h5>Buscar</h5>
+      </NavLink>
+      <NavLink className='Menu-link' to='/favorites' activeclassname="active">
+        <MdFavoriteBorder className='Menu-icons' />
+        <h5>Favoritos</h5>
+      </NavLink>
     </div>
   );
 }
