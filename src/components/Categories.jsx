2790fe87d645537ec178747f15e356f345690c18
Axel Torrico <torricohernan2@gmail.com>
2023-06-12 17:05:20 -0300

    agregue las categorias

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/Categories.css'
 
 export function Categories() {
+  const [categories, setCategories] = useState([])
+
+  const API_KEY = '763eb5981208a184a1e9429b05166e9c'
+  const url = `https://api.themoviedb.org/3/genre/movie/list?language=es&&api_key=${API_KEY}`
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data.genres);
+        setCategories(data.genres)
+      })
+      .catch((error) => {
+        console.log(error);
+    })
+  }, [])
+
   return (
     <div className='Categories'>
-      
+      <h2>Categorías</h2>
+      <div className='Categories-container'>
+        {categories.map((genres) => {
+          return (
+            <div className='Categories-content' key={genres.id}>
+              <h3>{genres.name}</h3>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
