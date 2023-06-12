54f52517409d9918f979b6f6ebd19c12ee13e5fd
Axel Torrico <torricohernan2@gmail.com>
2023-06-12 17:08:52 -0300

    llame a categorias

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,25 +1,16 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Header } from '../components/Header';
 import { Trending } from '../components/Trending';
+import { Categories } from '../components/Categories';
 import { Menu } from '../components/Menu'
 import '../styles/Home.css';
 
 function Home() {
-  /* const apiKey = import.meta.env.VITE_API_KEY;
-  const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${apiKey}`;
-
-  useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });
-  }, []); */
-
   return (
     <div className='Home'>
       <Header />
       <Trending />
+      <Categories />
       <Menu />
     </div>
   );
