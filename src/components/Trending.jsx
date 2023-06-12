10713fa93ce17550fd66ebd084113729107a4d19
Axel Torrico <torricohernan2@gmail.com>
2023-06-12 10:50:58 -0300

    maquete trending

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../styles/Trending.css'
 
+//images
+import imgAvatar from '../images/avatar_the_way_of_water.jpg'
+
 export function Trending() {
   return (
     <div className='Trending'>
       <h2>Tendencias</h2>
+      <div className='Trending-container'>
+        <img src={imgAvatar} alt="" />
+        <img src={imgAvatar} alt="" />
+        <img src={imgAvatar} alt="" />
+        <img src={imgAvatar} alt="" />
+        <img src={imgAvatar} alt="" />
+      </div>
     </div>
   );
 }
