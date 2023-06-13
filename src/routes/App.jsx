d113e10524e4a69e9352a71bbe4ee063460b3fad
Axel Torrico <torricohernan2@gmail.com>
2023-06-13 12:52:50 -0300

    cree una ruta para MoreTrendingMovies

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../containers/Home';
 import Search from '../containers/Search';
 import Favorites from '../containers/Favorites';
+import MoreTrendingMovies from '../containers/MoreTrendingMovies';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/search' element={<Search />} />
         <Route path='/favorites' element={<Favorites />} />
+        <Route path='/more-trending-movies' element={<MoreTrendingMovies />} />
       </Routes>
     </BrowserRouter>
   );
