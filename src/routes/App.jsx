0aff8545a908ac7c2f1d8ba6931428951908e766
Axel Torrico <torricohernan2@gmail.com>
2023-06-13 13:14:57 -0300

    cree una ruta para MoreTrendingTV

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,6 +4,7 @@ import Home from '../containers/Home';
 import Search from '../containers/Search';
 import Favorites from '../containers/Favorites';
 import MoreTrendingMovies from '../containers/MoreTrendingMovies';
+import MoreTrendingTV from '../containers/MoreTrendingTV';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path='/search' element={<Search />} />
         <Route path='/favorites' element={<Favorites />} />
         <Route path='/more-trending-movies' element={<MoreTrendingMovies />} />
+        <Route path='/more-trending-tv' element={<MoreTrendingTV />} />
       </Routes>
     </BrowserRouter>
   );
