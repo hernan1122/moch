a9e3ffe57e1549f9bb0fcf867edc3074da030492
Axel Torrico <torricohernan2@gmail.com>
2023-06-13 12:52:15 -0300

    cree el componente MoreTrendingMovies y sus estilos

---
+++ src/containers/MoreTrendingMovies.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Header } from '../components/Header';
+import { Menu } from '../components/Menu';
+import '../styles/More.css'
+
+//icons
+import {MdArrowBackIosNew} from 'react-icons/md'
+
+function MoreTrendingMovies() {
+  const [moreMovies, setMoreMovies] = useState([])
+
+  const API_KEY = '763eb5981208a184a1e9429b05166e9c'
+  const URL_IMAGE = 'https://image.tmdb.org/t/p/w300'
+  const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
+
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then((data) => {
+        setMoreMovies(data.results)
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }, [])
+
+  return (
+    <div className='More'>
+      <Header />
+      <div className='More-container-title'>
+        <h2>Peliculas tendencias</h2>
+        <Link to={'/'} className='More-link'>
+          Atras
+          <MdArrowBackIosNew className='More-icon' />
+        </Link>
+      </div>
+      <div className='More-container'>
+        {moreMovies.map((more) => {
+          return (
+            <div key={more.id}>
+              <img src={`${URL_IMAGE + more.poster_path}`} alt={`image: ${more.title}`} />
+            </div>
+          )
+        })}
+      </div>
+      <Menu />
+    </div>
+  );
+}
+
+export default MoreTrendingMovies;
