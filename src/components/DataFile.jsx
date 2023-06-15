d9f11a494e58b7b127eb97cb3e7d9a2992c94c45
Axel Torrico <torricohernan2@gmail.com>
2023-06-15 16:44:30 -0300

    elimine la const URL_IMAGE

--- src/components/DataFile.jsx
+++ src/components/DataFile.jsx
@@ -3,8 +3,6 @@ import { Link } from 'react-router-dom';
 import '../styles/DataFile.css'
 
 export function DataFile({ id, posterPath, title }) {
-  const URL_IMAGE = 'https://image.tmdb.org/t/p/w300'
-
   return (
     <Link to={`/details?id=${id}`} key={id}>
       <img className='DataFile-img' src={posterPath} alt={`image: ${title}`} />
