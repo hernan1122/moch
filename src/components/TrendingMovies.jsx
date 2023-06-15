32dcc4bda7719aad782ab3ddc2b8e47d80b4ebec
Axel Torrico <torricohernan2@gmail.com>
2023-06-15 16:45:53 -0300

    elimine al import de Link

--- src/components/TrendingMovies.jsx
+++ src/components/TrendingMovies.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import { DataFile } from './DataFile';
 import { ButtonMoreBack } from './ButtonMoreBack';
 import '../styles/ListHome.css'
