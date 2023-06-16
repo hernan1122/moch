4f5f57054ca6bfb1220195b353f25c6dd3d77c56
Axel Torrico <torricohernan2@gmail.com>
2023-06-16 10:38:54 -0300

    pase a 'to' como props

--- src/components/DataFile.jsx
+++ src/components/DataFile.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/DataFile.css'
 
-export function DataFile({ id, posterPath, title }) {
+export function DataFile({ id, to, posterPath, title }) {
   return (
-    <Link to={`/details?id=${id}`} key={id}>
+    <Link to={to} key={id}>
       <img className='DataFile-img' src={posterPath} alt={`image: ${title}`} />
     </Link>
   );
