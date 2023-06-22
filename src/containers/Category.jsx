7063c343af4a7be27c4cb65e1cb36e98f4dc5104
Axel Torrico <torricohernan2@gmail.com>
2023-06-22 10:00:43 -0300

    llame a header

--- src/containers/Category.jsx
+++ src/containers/Category.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Header } from '../components/Header';
 import '../styles/Category.css'
 
-export function Category() {
+function Category() {
   return (
     <div className='Category'>
-      
+      <Header />
+      <h2>Categoría</h2>
     </div>
   );
 }
+
+export default Category
